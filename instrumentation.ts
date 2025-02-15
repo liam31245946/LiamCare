d00b8dd7e86024caad28d74aa3405fa48621551f
liam nguyen <130803077+liam31245946@users.noreply.github.com>
2025-02-14 19:22:45 -0800

    change to directory

--- instrumentation.ts
+++ instrumentation.ts
@@ -4,6 +4,6 @@ export async function register() {
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
-    await import("../LiamCare/sentry.edge.config");
+    await import("./sentry.edge.config");
   }
 }
